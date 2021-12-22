7374cd17e864ee153fd89572171a00333746ff2b
Pavel Lomtev <p.v.lomtev@ya.ru>
2021-12-22 18:44:33 +0300

    Add CROSS_ENV

--- webpack/utils/devServer.ts
+++ webpack/utils/devServer.ts
@@ -6,16 +6,6 @@ import { isDev, rootDir } from './env'
  * @see https://webpack.js.org/configuration/dev-server/
  */
 export const devServerConfig = {
-  http2: true,
-  https: {
-    key: isDev && readFileSync(join(rootDir, 'certificate', 'key.pem')),
-    cert: isDev && readFileSync(join(rootDir, 'certificate', 'cert.pem'))
-  },
-  open: {
-    app: {
-      name: 'Google Chrome'
-    }
-  },
   port: 3000,
   host: 'local.psychoschool.ru',
   historyApiFallback: true
